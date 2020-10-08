790cc76744d31e15833e9a6666888f7147c92f7c
Burak Güldiken <burakguldiken1@gmail.com>
2020-10-08 20:09:32 +0300

    .net core console docker project

---
+++ dockerConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace dockerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            while(count < 100)
+            {
+                Console.WriteLine($"Hello Docker {count}");
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                count++;
+            }
+        }
+    }
+}
